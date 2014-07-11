f57103eae65999ab9c2b64f92f190fe0af64f379
Marcus Lindvall <twd.mir@hotmail.com>
2014-07-11 11:19:11 +0200

    Website fixed
    Removed wrong files and added the files that should be there.

---
+++ korv/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title>Korvfest</title>
+  <link rel="stylesheet" type="text/css" href="gif.css" />
+</head>
+
+<body>
+       <h1>Korvfest @ LURK LAN</br></h1>
+
+
+       <p margin="5px">Welcome to the new generation of LURK.</br>Here at LURK we've always missed the feeling of a good sausage.</br>That's why we are so happy to announce KORV-LAN2k14!</br>More info available soon.</p>
+</body>
+</html>
