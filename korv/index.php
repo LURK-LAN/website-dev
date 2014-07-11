8e9c53dbd8ad3ee217d9a508050e09428bdb3244
Marcus Lindvall <twd.mir@hotmail.com>
2014-07-11 12:11:26 +0200

    gif åwned v1.01
    Changed title, h1 and removed paragraph in index. Changed background and
    h1 in css.

--- korv/index.php
+++ korv/index.php
@@ -3,14 +3,13 @@
 <html>
 
 <head>
-  <title>Korvfest</title>
+  <title>Åwn3d(d0t)jpäg</title>
   <link rel="stylesheet" type="text/css" href="gif.css" />
 </head>
 
 <body>
-       <h1>Korvfest @ LURK LAN</br></h1>
 
+    <h1 align="center">Owned(dot)jpeg</h1>
 
-       <p margin="5px">Welcome to the new generation of LURK.</br>Here at LURK we've always missed the feeling of a good sausage.</br>That's why we are so happy to announce KORV-LAN2k14!</br>More info available soon.</p>
 </body>
 </html>
